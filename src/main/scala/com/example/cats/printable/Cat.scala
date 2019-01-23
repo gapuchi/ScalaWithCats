4c32b04ed289dce81e308d334dd81091f916ea89
Arjun Adhia <arjun.adhia@gmail.com>
2019-01-23 07:11:52 -0800

    Added Cat case class example and interface syntax.

---
+++ src/main/scala/com/example/cats/printable/Cat.scala
@@ -0,0 +1,3 @@
+package com.example.cats.printable
+
+final case class Cat(name: String, age: Int, color: String)
--- src/main/scala/com/example/cats/printable/PrintableInstances.scala
+++ src/main/scala/com/example/cats/printable/PrintableInstances.scala
@@ -4,4 +4,5 @@ object PrintableInstances {
   //Defining using single abstract method
   implicit val printableString: Printable[String] = (value: String) => value
   implicit val printableInt: Printable[Int] = (value: Int) => value.toString
+  implicit val printableCat: Printable[Cat] = (cat: Cat) => s"${cat.name} is a ${cat.age} year-old ${cat.color} cat."
 }
